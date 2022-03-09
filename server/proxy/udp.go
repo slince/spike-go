887ee692efe516e2cf3bf2955efc0fbfce64f4a6
slince <taosikai@yeah.net>
2022-03-10 01:14:04 +0800

    contracts

--- server/proxy/udp.go
+++ server/proxy/udp.go
@@ -29,49 +29,53 @@ func (udp *UdpHandler) Listen(serverPort int) error {
 	if err != nil {
 		return err
 	}
-	var proxyConn = udp.proxyConnPool.Get()
-	var bridge = cmd.NewBridge(proxyConn)
 
 	go func() {
-		buf := make([]byte, 1024)
-		for {
-			read, remoteAddr, _ := udpConn.ReadFromUDP(buf)
-			if read == 0 {
-				break
-			}
-			var udpPackage = &cmd.UdpPackage{
-				Body: buf[0:read],
-				RemoteAddr: remoteAddr,
-			}
-			err = bridge.Write(udpPackage)
-			if err != nil {
-				break
-			}
-		}
-		udpConn.Close()
-		proxyConn.Close()
-	}()
+		var proxyConn = udp.proxyConnPool.Get()
+		var bridge = cmd.NewBridge(proxyConn)
 
-	go func() {
-		Handle:
-		for {
-			var command, err = bridge.Read()
-			if err != nil {
-				break
-			}
-			switch command := command.(type) {
-			case *cmd.UdpPackage:
-				_, err = udpConn.WriteToUDP(command.Body, command.RemoteAddr)
+		go func() {
+			buf := make([]byte, 1024)
+			for {
+				read, remoteAddr, _ := udpConn.ReadFromUDP(buf)
+				if read == 0 {
+					break
+				}
+				var udpPackage = &cmd.UdpPackage{
+					Body: buf[0:read],
+					RemoteAddr: remoteAddr,
+				}
+				err = bridge.Write(udpPackage)
 				if err != nil {
+					break
+				}
+			}
+			udpConn.Close()
+			proxyConn.Close()
+		}()
+
+		go func() {
+		Handle:
+			for {
+				var command, err = bridge.Read()
+				if err != nil {
+					break
+				}
+				switch command := command.(type) {
+				case *cmd.UdpPackage:
+					_, err = udpConn.WriteToUDP(command.Body, command.RemoteAddr)
+					if err != nil {
+						break Handle
+					}
+				default:
 					break Handle
 				}
-			default:
-				break Handle
 			}
-		}
-		udpConn.Close()
-		proxyConn.Close()
+			udpConn.Close()
+			proxyConn.Close()
+		}()
 	}()
+
 	return nil
 }
 
